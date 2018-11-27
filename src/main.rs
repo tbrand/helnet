9ed922f9c1bffe4d22f4e87831aa7d8ee7fd74dd
tbrand <taichiro0709@gmail.com>
2018-11-27 23:13:08 +0900

    add trust-dns-resolver to Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate trust_dns_resolver;
+
+use trust_dns_resolver::Resolver;
+use trust_dns_resolver::config::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
+    let mut response = resolver.lookup_ip("www.example.com.").unwrap();
+    println!("{:?}", response);
 }
