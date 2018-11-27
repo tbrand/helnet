a92e10b197d8ff562ac6f55ed786b8451b11f9ed
tbrand <taichiro0709@gmail.com>
2018-11-27 23:40:31 +0900

    detect v4 or v6

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 extern crate trust_dns_resolver;
+extern crate clap;
 
 use trust_dns_resolver::Resolver;
 use trust_dns_resolver::config::*;
+use std::net::IpAddr;
 
 fn main() {
-    let mut resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
-    let mut response = resolver.lookup_ip("www.example.com.").unwrap();
-    println!("{:?}", response);
+    let matches = clap::App::new(clap::crate_name!())
+        .about(clap::crate_description!())
+        .author(clap::crate_authors!())
+        .version(clap::crate_version!())
+        .get_matches();
+
+    let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
+    let response = resolver.lookup_ip("www.example.com.").unwrap();
+
+    for ip in response.iter() {
+        match ip {
+            IpAddr::V4(ip) => {
+                println!("It's IPv4 {:?}", ip);
+            }
+            IpAddr::V6(ip) => {
+                println!("It's IPv6 {:?}", ip);
+            }
+        }
+    }
 }
